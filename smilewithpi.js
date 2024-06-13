f14655addcda81ee18933175e5eb8669378e9970
Ruqayya2024 <157078776+Ruqayya2024@users.noreply.github.com>
2024-06-13 14:13:30 -0500

    Update smilewithpi.js

--- smilewithpi.js
+++ smilewithpi.js
@@ -1,1 +1,26 @@
+ctx.beginPath();
+ctx.arc(320,320,100,0, 2*Math.PI);
+ctx.stroke();
+ctx.fillStyle = 'yellow';
+ctx.fill();
+
+//ctx.closePath();
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.moveTo(240,250);
+ctx.fillStyle = 'black'
+ctx.arc(200,250,40,0,2*Math.PI);
+
+//ctx.closePath();
+//ctx.beginPath();
+
+
+//ctx.closePath();
+//ctx.beginPath();
+
+ctx.moveTo(440,250);
+ctx.arc(400,250,40,0,2*Math.PI);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
